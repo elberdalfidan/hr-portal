3ca7eb114a27f07baa1a7cbdeb986533f27fd698
Elber Dalfidan <elberdalfidan@gmail.com>
2024-12-08 15:07:38 +0300

    fix: swagger DEFAULT_SCHEMA_CLASS error

--- apps/notifications/api/views.py
+++ apps/notifications/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from django.utils import timezone
-from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiExample
+from drf_spectacular.utils import extend_schema
 from ..models import Notification
 from .serializers import NotificationSerializer
 
